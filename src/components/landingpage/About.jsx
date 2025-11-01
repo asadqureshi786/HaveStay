d1ba3d1eaae3b5d133c66037aae751adf45bdd46
Asad Qureshi <asadqureshi786>
2025-10-31 21:36:51 -0700

    update

--- src/components/landingpage/About.jsx
+++ src/components/landingpage/About.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import Img1 from "../../assets/img/website/about_img/img1.jpg"
+
+// Icons
+import { IoIosArrowRoundForward } from "react-icons/io";
+
 
 export default function About() {
   return (
@@ -10,10 +15,14 @@ export default function About() {
 
     <div className="grid grid-cols-2">
         <div className="left_side">
-            <p className="p-5 text-[14px]">
-                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal it look like readable English. Many desktop publishing packages and web page editors now use Various versions have evolved over the years, sometimes by accident.
-                <img src={} />
+            <p className="px-8 pb-5">
+                <div className="p-7" >
+                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal it look like readable English. Many desktop publishing packages and web page editors now use Various versions have evolved over the years, sometimes by accident.
+                <button type='button' className=' bg-transparent text-black border-0 btn btn-secondary' >Read About Us <span className='icon text-black' ><IoIosArrowRoundForward/> </span></button>
+                </div>
+                
             </p>
+                <img src={Img1} className="rounded-[20px] w-full h-[250px] object-cover" />
         </div>
     </div>
 
